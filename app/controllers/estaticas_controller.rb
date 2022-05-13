452ed81a6d6a8daaec371555374f3e1c6a057e25
Marco Retamales <marcoret74@gmail.com>
2022-05-13 16:47:12 -0400

    añadimos estaticas

---
+++ app/controllers/estaticas_controller.rb
@@ -0,0 +1,10 @@
+class EstaticasController < ApplicationController
+  def contacto
+  end
+
+  def nosotros
+  end
+
+  def otra
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get "estaticas/contacto"
+  get "estaticas/nosotros"
+  get "estaticas/otra"
 
   resources :personas
-  get 'articulos/new'
-  get 'articulos/index'
-  get 'articulos/show'
+  get "articulos/new"
+  get "articulos/index"
+  get "articulos/show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
